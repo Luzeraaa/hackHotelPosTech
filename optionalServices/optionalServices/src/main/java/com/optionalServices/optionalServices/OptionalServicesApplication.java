038623eab9201520125cc67d37ba6984e7dc770c
Deco <deco.neto10@gmail.com>
2024-03-11 21:45:30 -0300

    initial commit

---
+++ optionalServices/optionalServices/src/main/java/com/optionalServices/optionalServices/OptionalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.optionalServices.optionalServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionalServicesApplication.class, args);
+	}
+
+}
---
+++ optionalServices/optionalServices/src/test/java/com/optionalServices/optionalServices/OptionalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optionalServices.optionalServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
